1a15e3314f7703ee2f9b0e7c769319ca42f036bf
danielalej <36351967+danielalej@users.noreply.github.com>
2022-03-26 22:46:26 -0500

    Header, Sections
    index.html
    - headers updated
    app.js
    - active code for nav bar
    styles.scss
    - header code updated
    - .active transform code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const sections = document.querySelectorAll('.section')
-const sectionBtns = document.querySelectorAll('.controls')
-const sectionBtn = document.querySelectorAll('.control')
-const allSections = document.querySelectorAll('.main-content')
+const sections = document.querySelectorAll('.section');
+const sectionBtns = document.querySelectorAll('.controls');
+const sectionBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
 
 function pageTransition() {
     //Button click active class
@@ -13,5 +13,29 @@ function pageTransition() {
             btn.className += ' active-btn';
         })
     }
+
+    //Sections Active Class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove selected from other btns
+            //sectionBtns.forEach((btn) => {
+            //    btn.classList.remove('active')
+            //})
+            const btns = sectionBtn[0].children;
+            for (let b of btns){
+                b.classList.remove('active')
+            }
+            e.target.classList.add('active')
+
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 pageTransition();
